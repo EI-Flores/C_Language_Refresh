9bfd0c6dee4c9621e9f71cd5fccd0ce369d24205
Juan2 <47181714+EI-Flores@users.noreply.github.com>
2025-08-24 19:39:37 +0000

    feat: add hello world from Codespaces

---
+++ src/basics/hello_codespace.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello, I am Juan\n");
+    printf("This is my first proman from Github Codespaces.\n");
+    printf("I am now coding from my Tablet\n");
+    return 0;
+}
